e9d705e0f4a98ac365738d924fca67c5fc0d6317
ваше имя <cyril.lee1997@gmail.com>
2023-01-21 20:14:14 +0200

    in progress

--- src/handleInput.js
+++ src/handleInput.js
@@ -1,11 +1,11 @@
 import refs from "./refs";
 import State from "./state";
 
-// refs.input.addEventListener("input", (e) => {
-//   //   state.state.places.filter((place) => {
-//   //     place.name.includes(e.target.value);
-//   //   });
-//   const a = new stateManager();
+refs.input.addEventListener("input", (e) => {
+  //   state.state.places.filter((place) => {
+  //     place.name.includes(e.target.value);
+  //   });
+  const a = new stateManager();
 
-//   console.log(a);
-// });
+  console.log(a);
+});
